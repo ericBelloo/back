3e925e3d1c083e5a7377c002706e6b605bad5896
eric bello <ebelloortiz@gmail.com>
2020-12-18 00:01:27 -0600

    fix

--- back/settings.py
+++ back/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'back.urls'
