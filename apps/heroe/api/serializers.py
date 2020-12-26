26f2f467e80f31a6c1da494754da403ecc7e6cee
eric bello <ebelloortiz@gmail.com>
2020-12-25 21:20:27 -0600

    add heroe detail api

--- apps/heroe/api/serializers.py
+++ apps/heroe/api/serializers.py
@@ -2,10 +2,10 @@
 # Rest Framework
 from rest_framework import serializers
 # Models
-from apps.heroe.models import Heroes
+from apps.heroe.models import Heroes, Editorial
 
 
-class HeroesSerializer(serializers.ModelSerializer):
+class HeroesListSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(required=True, allow_null=False)
     name = serializers.CharField(required=True, allow_null=True)
     description = serializers.CharField(required=True, allow_null=True)
@@ -14,3 +14,24 @@ class HeroesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Heroes
         fields = ('id', 'name', 'description', 'apparition')
+
+
+class EditorialSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=True, allow_null=False)
+    name = serializers.CharField(required=True, allow_null=False)
+
+    class Meta:
+        model = Editorial
+        fields = ('id', 'name')
+
+
+class HeroeDetailSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=True, allow_null=False)
+    name = serializers.CharField(required=True, allow_null=True)
+    description = serializers.CharField(required=True, allow_null=True)
+    apparition = serializers.DateField(required=True, allow_null=True)
+    editorial = EditorialSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Heroes
+        fields = ('id', 'name', 'description', 'apparition', 'editorial')
--- apps/heroe/api/urls.py
+++ apps/heroe/api/urls.py
@@ -1,10 +1,11 @@
 
 
 from django.urls import path
-from apps.heroe.api.views import HeroesList
+from apps.heroe.api.views import HeroesList, HeroeDetail
 
 app_name = 'heroe_api'
 
 urlpatterns = [
     path('heroes-list/', HeroesList.as_view(), name='get_heroes_list'),
+    path('heroe/', HeroeDetail.as_view(), name='get_heroe'),
 ]
